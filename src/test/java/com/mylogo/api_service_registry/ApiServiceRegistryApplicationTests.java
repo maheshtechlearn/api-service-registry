7b6fd6b2aa8fb24826f91345deba70cf69ebc714
Mahesh Tekumani <tekumani.mahesh2@academy.com>
2024-11-12 11:05:47 +0530

    Initial commit

---
+++ src/test/java/com/mylogo/api_service_registry/ApiServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylogo.api_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
